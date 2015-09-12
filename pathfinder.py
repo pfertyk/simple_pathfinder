bc1ded29095a82cd06161b71a4a852a48d25659a
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-09-12 19:24:59 +0200

    2 more failing tests.

--- pathfinder.py
+++ pathfinder.py
@@ -1,10 +1,14 @@
+from collections import namedtuple
 
 __author__ = 'pawel'
 
+RectangularObstacle = namedtuple('RectangularObstacle', 'up down left right')
+# TODO: better names for fields in Unit?
+RectangularUnit = namedtuple('RectangularUnit', 'middle_x, middle_y, size_x, size_y')
 
 class Pathfinder:
     @staticmethod
-    def find_path(start, destination, obstacles):
+    def find_path(unit, destination, obstacles):
         return [destination]
 
 if __name__ == '__main__':
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,17 +1,32 @@
 from unittest import TestCase
-from pathfinder import Pathfinder
+from pathfinder import Pathfinder, RectangularObstacle, RectangularUnit
 
 __author__ = 'pawel'
 
 
 class TestPathfinder(TestCase):
     def test_no_obstacles(self):
-        start = (0, 0)
-        destination = (0, 3)
+        unit = RectangularUnit(0, 0, 0.5, 0.5)
+        destination = (3, 0)
         obstacles = []
+        path = Pathfinder().find_path(unit, destination, obstacles)
+        self.assertEqual(path, [(3, 0)])
 
-        path = Pathfinder().find_path(start, destination, obstacles)
+    def test_one_obstacle(self):
+        unit = RectangularUnit(0, 0, 0.5, 0.5)
+        destination = (5, 0)
+        obstacles = [RectangularObstacle(1, -3, 2, 3)]
+        path = Pathfinder().find_path(unit, destination, obstacles)
+        self.assertEquals(path, [(1.5, 1.5), (3.5, 1.5), (5, 0)])
 
-        self.assertEqual(path, [(0,3)])
-
-
+    def test_no_path(self):
+        unit = RectangularUnit(0, 0, 0.5, 0.5)
+        destination = (3, 0)
+        obstacles = [
+            RectangularObstacle(1, -1, 1, 2),
+            RectangularObstacle(1, -1, 4, 5),
+            RectangularObstacle(2, 1, 2, 4),
+            RectangularObstacle(-1, -2, 2, 4)
+        ]
+        path = Pathfinder().find_path(unit, destination, obstacles)
+        self.assertEquals(path, None)
