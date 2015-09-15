bdbdde64c623be697e7951872fa83a88d8460860
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-09-15 21:39:51 +0200

    Method for obstacle crossing detection and tests.

--- pathfinder.py
+++ pathfinder.py
@@ -1,15 +1,62 @@
 from collections import namedtuple
+import numpy as np
 
-__author__ = 'pawel'
-
-RectangularObstacle = namedtuple('RectangularObstacle', 'up down left right')
 # TODO: better names for fields in Unit?
+# TODO: svg visualization of found paths
+# TODO: turn off warning 'divide by zero'
+RectangularObstacle = namedtuple('RectangularObstacle', 'up down left right')
 RectangularUnit = namedtuple('RectangularUnit', 'middle_x, middle_y, size_x, size_y')
+Point = namedtuple('Point', 'x y')
+
 
 class Pathfinder:
     @staticmethod
     def find_path(unit, destination, obstacles):
+        new_obstacles = [
+            RectangularObstacle(o.up - unit.size_y, o.down + unit.size_y, o.left - unit.size_x, o.right + unit.size_x)
+            for o in obstacles]
+        # build graph
+        # find path
         return [destination]
+
+    @staticmethod
+    def line_crosses_obstacle(p1, p2, obstacle):
+        if p1 == p2:
+            return False
+
+        e = p1
+        d = np.subtract(p2, p1)
+        d_len = np.linalg.norm(d)
+        d = np.divide(d, d_len)
+        d = Point(*d)
+
+        ax = np.divide(1, d.x)
+        ay = np.divide(1, d.y)
+
+        if ax >= 0:
+            txmin = np.multiply(ax, obstacle.left - e.x)
+            txmax = np.multiply(ax, obstacle.right - e.x)
+        else:
+            txmin = np.multiply(ax, obstacle.right - e.x)
+            txmax = np.multiply(ax, obstacle.left - e.x)
+
+        if ay >= 0:
+            tymin = np.multiply(ay, obstacle.up - e.y)
+            tymax = np.multiply(ay, obstacle.down - e.y)
+        else:
+            tymin = np.multiply(ay, obstacle.down - e.y)
+            tymax = np.multiply(ay, obstacle.up - e.y)
+
+        # return not (txmin >= tymax or tymin >= txmax)
+        return txmin < tymax and tymin < txmax and txmin < d_len and tymin < d_len and txmax > 0 and tymax > 0
 
 if __name__ == '__main__':
     print('Hello!')
+    print(np.divide(1, 0))
+    print(np.divide(-1.0, 0.0))
+    print(np.divide(0.0, 0.0))
+    p = Point(4, 3)
+    l = np.linalg.norm(p)
+    print(l)
+    p /= l
+    print(p)
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,10 +1,8 @@
 from unittest import TestCase
-from pathfinder import Pathfinder, RectangularObstacle, RectangularUnit
-
-__author__ = 'pawel'
+from pathfinder import Pathfinder, RectangularObstacle, RectangularUnit, Point
 
 
-class TestPathfinder(TestCase):
+class TestFindingPath(TestCase):
     def test_no_obstacles(self):
         unit = RectangularUnit(0, 0, 0.5, 0.5)
         destination = (3, 0)
@@ -15,7 +13,7 @@ class TestPathfinder(TestCase):
     def test_one_obstacle(self):
         unit = RectangularUnit(0, 0, 0.5, 0.5)
         destination = (5, 0)
-        obstacles = [RectangularObstacle(1, -3, 2, 3)]
+        obstacles = [RectangularObstacle(-3, 1, 2, 3)]
         path = Pathfinder().find_path(unit, destination, obstacles)
         self.assertEquals(path, [(1.5, 1.5), (3.5, 1.5), (5, 0)])
 
@@ -23,10 +21,132 @@ class TestPathfinder(TestCase):
         unit = RectangularUnit(0, 0, 0.5, 0.5)
         destination = (3, 0)
         obstacles = [
-            RectangularObstacle(1, -1, 1, 2),
-            RectangularObstacle(1, -1, 4, 5),
-            RectangularObstacle(2, 1, 2, 4),
-            RectangularObstacle(-1, -2, 2, 4)
+            RectangularObstacle(-1, 1, 1, 2),
+            RectangularObstacle(-1, 1, 4, 5),
+            RectangularObstacle(1, 2, 2, 4),
+            RectangularObstacle(-2, -1, 2, 4)
         ]
         path = Pathfinder().find_path(unit, destination, obstacles)
         self.assertEquals(path, None)
+
+
+class TestFindingPath(TestCase):
+    def test_through_the_middle(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 0)
+        p2 = Point(4, 4)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertTrue(result)
+
+    def test_through_the_middle_x(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 2)
+        p2 = Point(4, 2)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertTrue(result)
+
+    def test_through_the_middle_y(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(2, 0)
+        p2 = Point(2, 4)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertTrue(result)
+
+    def test_through_the_middle_reversed(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(4, 4)
+        p2 = Point(0, 0)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertTrue(result)
+
+    def test_through_the_middle_different_angle(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 2.5)
+        p2 = Point(3, 3.99)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertTrue(result)
+
+    def test_no_collision(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 0)
+        p2 = Point(4, 0)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_parallel_edge_x(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 1)
+        p2 = Point(4, 1)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_parallel_edge_y(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(1, 0)
+        p2 = Point(1, 4)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_tip(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 2)
+        p2 = Point(2, 4)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_starting_in_tip(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(1, 1)
+        p2 = Point(0, 0)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_starting_in_tip_perpendicular(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(1, 1)
+        p2 = Point(0, 1)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_touching_side_perpendicular(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(0, 2)
+        p2 = Point(1, 2)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_not_crossing_but_pointing_perpendicular(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(-1, 2)
+        p2 = Point(0, 2)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_not_crossing_but_pointing(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(-1, 2.5)
+        p2 = Point(0, 2)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
+
+    def test_pointing_away(self):
+        obstacle = RectangularObstacle(1, 3, 1, 3)
+        p1 = Point(4, 2)
+        p2 = Point(5, 2.5)
+
+        result = Pathfinder.line_crosses_obstacle(p1, p2, obstacle)
+        self.assertFalse(result)
