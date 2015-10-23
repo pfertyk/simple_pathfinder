1afe209c995b12846899a91d8441ae84abb7a2fc
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-10-23 22:42:24 +0200

    Refactored names in gui.

--- pathfinder_gui.py
+++ pathfinder_gui.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 
-class MovingObjects(object):
+class PathfinderGUI(object):
     def __init__(self):
         self.obstacles = (
             RectangularObstacle(70, 90, 70, 700),
@@ -18,35 +18,38 @@ class MovingObjects(object):
             RectangularObstacle(290, 310, 290, 550),
             RectangularObstacle(310, 500, 500, 520),
         )
-        self.player = RectangularUnit(Point(30, 30), 25, 25)
+
+        self.agent = RectangularUnit(Point(30, 30), 25, 25)
         self.velocity = 8.0
         self.path = []
         self.fps = 25
 
         self.master = Tk()
         self.master.wm_title('Left click to point destination, Esc to exit')
+        self.master.after(0, self.animate)
+
         self.canvas = Canvas(self.master, width=800, height=600)
-        self.canvas.pack()
 
         for ob in self.obstacles:
             self.canvas.create_rectangle(ob.left, ob.up, ob.right, ob.down, fill="blue")
+
         self.player_rectangle = self.canvas.create_rectangle(
-            self.player.position.x - self.player.size_x,
-            self.player.position.y - self.player.size_y,
-            self.player.position.x + self.player.size_x,
-            self.player.position.y + self.player.size_y, fill="green")
+            self.agent.position.x - self.agent.size_x,
+            self.agent.position.y - self.agent.size_y,
+            self.agent.position.x + self.agent.size_x,
+            self.agent.position.y + self.agent.size_y, fill="green")
 
         self.canvas.pack()
-        self.master.after(0, self.animate)
         self.canvas.bind('<Button-1>', self.clicked)
-        self.canvas.master.bind('<Escape>', self.close)
+        self.canvas.master.bind('<Escape>', self.close_window)
         self.canvas.focus_set()
+
         self.master.mainloop()
 
     def animate(self):
         if self.path:
             next_point = self.path[0]
-            d = np.subtract(next_point, self.player.position)
+            d = np.subtract(next_point, self.agent.position)
             d_len = np.linalg.norm(d)
 
             if d_len < self.velocity:
@@ -55,23 +58,23 @@ class MovingObjects(object):
                 d = np.divide(d, d_len)
                 d = np.multiply(d, self.velocity)
 
-            new_position = np.add(self.player.position, d)
+            new_position = np.add(self.agent.position, d)
             new_position = Point(*new_position)
-            int_x = int(new_position.x) - int(self.player.position.x)
-            int_y = int(new_position.y) - int(self.player.position.y)
-            self.player = RectangularUnit(new_position, self.player.size_x, self.player.size_y)
+            int_x = int(new_position.x) - int(self.agent.position.x)
+            int_y = int(new_position.y) - int(self.agent.position.y)
+            self.agent = RectangularUnit(new_position, self.agent.size_x, self.agent.size_y)
             self.canvas.move(self.player_rectangle, int_x, int_y)
         self.master.after(int(1000 / self.fps), self.animate)
 
     def clicked(self, event):
         print('Clicked: ', event.x, event.y)
         start = time.time()
-        self.path = find_path(self.player, Point(event.x, event.y), self.obstacles)
+        self.path = find_path(self.agent, Point(event.x, event.y), self.obstacles)
         end = time.time()
         print(end-start)
 
-    def close(self, event):
+    def close_window(self, event):
         self.master.destroy()
 
 if __name__ == '__main__':
-    MovingObjects()
+    PathfinderGUI()
