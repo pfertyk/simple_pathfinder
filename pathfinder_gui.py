f09b0b2a0c4e4edfbb659a023e65308f731460f1
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-10-24 11:11:03 +0200

    Moved obstacle creation to a separate function, small refactoring.

--- pathfinder_gui.py
+++ pathfinder_gui.py
@@ -5,43 +5,38 @@ import time
 
 class PathfinderGUI(object):
     def __init__(self):
-        self.obstacles = (
-            RectangularObstacle(70, 90, 70, 700),
-            RectangularObstacle(90, 510, 70, 90),
-            RectangularObstacle(510, 530, 70, 400),
-            RectangularObstacle(90, 300, 680, 700),
-            RectangularObstacle(370, 540, 680, 700),
-            RectangularObstacle(150, 360, 160, 180),
-            RectangularObstacle(150, 170, 180, 380),
-            RectangularObstacle(150, 290, 380, 400),
-            RectangularObstacle(290, 310, 290, 550),
-            RectangularObstacle(310, 500, 500, 520),
-        )
-
+        self.obstacles = create_sample_obstacles()
         self.agent = RectangularUnit(Point(30, 30), 25, 25)
         self.velocity = 8.0
         self.path = []
         self.fps = 25
-
-        self.master = Tk()
-        self.master.wm_title('Left click to point destination, Esc to exit')
-
-        self.canvas = Canvas(self.master, width=800, height=600)
-
-        for ob in self.obstacles:
-            self.canvas.create_rectangle(ob.left, ob.up, ob.right, ob.down, fill='blue')
-
-        self.player_rectangle = self.canvas.create_rectangle(0, 0, 1, 1, fill='green', tags='agent')
-        self.redraw_agent()
-
-        self.canvas.pack()
-        self.canvas.bind('<Button-1>', self.calculate_new_path)
-        self.canvas.master.bind('<Escape>', self.close_window)
-        self.canvas.focus_set()
-
         self.is_drawing = False
 
+        self.master = Tk()
+        self.master.wm_title('Left click to select destination, Esc to exit')
+        self.master.bind('<Escape>', self.close_window)
+
+        self.canvas = Canvas(self.master, width=800, height=600)
+        self.player_rectangle = self.canvas.create_rectangle(0, 0, 1, 1, fill='green', tags='agent')
+        self.init_canvas()
+
         self.master.mainloop()
+
+    def init_canvas(self):
+        for ob in self.obstacles:
+            self.canvas.create_rectangle(ob.left, ob.up, ob.right, ob.down, fill='blue')
+        self.redraw_agent()
+        self.canvas.pack()
+        self.canvas.bind('<Button-1>', self.calculate_new_path)
+        self.canvas.focus_set()
+
+    def redraw_agent(self):
+        self.canvas.coords(
+            'agent',
+            self.agent.position.x - self.agent.size_x,
+            self.agent.position.y - self.agent.size_y,
+            self.agent.position.x + self.agent.size_x,
+            self.agent.position.y + self.agent.size_y)
 
     def move_agent_along_path(self):
         self.is_drawing = True
@@ -57,14 +52,6 @@ class PathfinderGUI(object):
         else:
             self.is_drawing = False
 
-    def redraw_agent(self):
-        self.canvas.coords(
-            'agent',
-            self.agent.position.x - self.agent.size_x,
-            self.agent.position.y - self.agent.size_y,
-            self.agent.position.x + self.agent.size_x,
-            self.agent.position.y + self.agent.size_y)
-
     def calculate_new_path(self, event):
         print('Selected destination: ', event.x, event.y)
         destination = Point(event.x, event.y)
@@ -79,6 +66,22 @@ class PathfinderGUI(object):
 
     def close_window(self, event):
         self.master.destroy()
+
+
+def create_sample_obstacles():
+    return (
+            RectangularObstacle(70, 90, 70, 700),
+            RectangularObstacle(90, 510, 70, 90),
+            RectangularObstacle(510, 530, 70, 400),
+            RectangularObstacle(90, 300, 680, 700),
+            RectangularObstacle(370, 540, 680, 700),
+            RectangularObstacle(150, 360, 160, 180),
+            RectangularObstacle(150, 170, 180, 380),
+            RectangularObstacle(150, 290, 380, 400),
+            RectangularObstacle(290, 310, 290, 550),
+            RectangularObstacle(310, 500, 500, 520),
+        )
+
 
 if __name__ == '__main__':
     PathfinderGUI()
